f2c4590a198e544408171884baf880e39ace4269
Timothy Edwards <91966621+timy352@users.noreply.github.com>
2021-11-03 12:30:38 +0000

    Add files via upload
    The demonstration file for this class

---
+++ textdemo.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<LINK REL="STYLESHEET" TYPE="text/css" HREF="/word-htm.css?id=<?= time() ?>" media="screen">
+
+</head>
+
+<body>
+<?php
+require_once('wordtext.php');
+$rt = new WordTEXT(false,'UTF-8');
+$text = $rt->readDocument('sample.docx');
+
+$det = explode(':',$text[0]);
+echo "No of text elements in the array - ".$det[0]."<br>";
+echo "Max length of a text element in the array - ".$det[1]."<br>&nbsp;<br>";
+$LC = 1;
+while ($LC <= $det[0]){
+	echo "Element ".$LC." : ".$text[$LC]."<br>";
+	$LC++;
+}
+
+?>
+</body>
